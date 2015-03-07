21d759e174f3a48f851018573cb055e0d728fde6
Mike Gaal <mikegaal@microsoft.com>
2015-03-06 17:28:53 -0800

    Update finalconfig.sh

--- bash/finalconfig.sh
+++ bash/finalconfig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #On your management node logged in as root
-chmod 755 hostscript.sh
-chmod 755 updatehosts.sh
+chmod 755 /root/hostscript.sh
+chmod 755 /root/updatehosts.sh
 #Reads the hosts.txt and updates /etc/hosts file on the management node
-sh updatehosts.sh
+sh /root/updatehosts.sh
 #Update the hosts file on each node in the cluster and mounts the data drives
-sh hostscript.sh
+sh /root/hostscript.sh
