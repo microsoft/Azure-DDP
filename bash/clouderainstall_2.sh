9c3970fca86faffce52d76ddbb8cc4f380d5d95e
Mike Gaal <mikegaal@microsoft.com>
2015-03-09 12:32:48 -0700

    Update clouderainstall_2.sh

--- bash/clouderainstall_2.sh
+++ bash/clouderainstall_2.sh
@@ -2,5 +2,5 @@ wget http://archive.cloudera.com/cm5/redhat/6/x86_64/cm/cloudera-manager.repo -P
 yum clean all
 yum -y install oracle-j2sdk1.7
 yum -y install cloudera-manager-server-db-2
-service cloudera-scm-server-db-start
+service cloudera-scm-server-db start
 service cloudera-scm-server start
