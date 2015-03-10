be91c44993f4aadaf28425409808baf0f0f51064
Mike Gaal <mikegaal@microsoft.com>
2015-03-10 12:02:44 -0700

    Create provisioncluster.sh

---
+++ bash/provisioncluster.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+source ./clustersetup.sh
+mgmtnode="${vmNamePrefix}0"
+
+echo Sleeping for 30 seconds...
+sleep 30
+azure vm extension set -c "./final_config.json" $mgmtnode CustomScriptForLinux Microsoft.OSTCExtensions 1.*
+echo Formatting and mounting disks on all cluster nodes...pausing 5 minutes
+sleep 300
+azure vm extension set -c "./cloudera_install.json" $mgmtnode CustomScriptForLinux Microsoft.OSTCExtensions 1.*
+echo Cloudera Installation submitted - please wait up to 5 minutes and browse to http://$cloudServicePrefix.cloudapp.net:7180 to finalize configuration
