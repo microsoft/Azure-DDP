4d898068645fe3f5325673936399c7756747d338
Mike Gaal <mikegaal@microsoft.com>
2015-03-10 12:16:22 -0700

    Update provisioncluster.sh

--- bash/provisioncluster.sh
+++ bash/provisioncluster.sh
@@ -9,3 +9,11 @@ echo Formatting and mounting disks on all cluster nodes...pausing 5 minutes
 sleep 300
 azure vm extension set -c "./cloudera_install.json" $mgmtnode CustomScriptForLinux Microsoft.OSTCExtensions 1.*
 echo Cloudera Installation submitted - please wait up to 5 minutes and browse to http://$cloudServicePrefix.cloudapp.net:7180 to finalize configuration
+echo Deleting SSH endpoints on cluster nodes for security...
+countService=1
+cloudServiceName="$clustercloudServicePrefix$countService"
+vmlist=$(azure vm list -d $cloudServiceName --json | jq .[] | jq ."VMName")
+vmlistclean=$(echo $vmlist | sed 's/\"//g')
+for i in $vmlistclean; do
+    azure vm endpoint delete $i ssh
+done
